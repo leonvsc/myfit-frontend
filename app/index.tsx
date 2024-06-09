d9d0a902d454d45c0d99032d588a2b5da4f79266
Leon <leonvsc@users.noreply.github.com>
2024-06-09 11:59:30 +0200

    Fix: Removed main.yml and changed versions on cd.yml

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-amber-600">
-      <Text className="text-3xl">Hello World!!!!!!!!!!!!!!!!!!!</Text>
+      <Text className="text-3xl">Hello World!</Text>
     </View>
   );
 }
