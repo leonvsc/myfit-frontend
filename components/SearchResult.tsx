78ad3923b5b00563b57467f58c1f6f51e7e16dfc
Leon <leonvsc@users.noreply.github.com>
2024-06-25 18:37:59 +0200

    Feat: Designed search page

---
+++ components/SearchResult.tsx
@@ -0,0 +1,27 @@
+import {View, Text, Image, TouchableOpacity} from "react-native";
+import React from "react";
+
+type SearchResultProps = {
+    icon: any,
+    handlePress: any
+}
+
+const SearchResult: React.FC<SearchResultProps> = ({icon, handlePress}) => {
+    return (
+    <TouchableOpacity
+        className="flex-row items-center bg-gray-200 rounded-xl w-full h-16 px-4 mb-5"
+        onPress={handlePress}
+        activeOpacity={0.7}
+    >
+        <Image
+            source={icon}
+            resizeMode="contain"
+            className="w-10 h-10"/>
+        <Text className="ml-5 text-lg font-pmedium">
+            SearchResult
+        </Text>
+    </TouchableOpacity>
+    )
+}
+
+export default SearchResult;
