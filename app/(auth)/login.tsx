6522b0a8e03dda13393bd5763ba2ae2e5e73b905
Leon <leonvsc@users.noreply.github.com>
2024-06-27 15:04:33 +0200

    Fix: Disabled login so the app can start again

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,31 +1,30 @@
-import {useEffect, useCallback, useMemo} from "react";
-import {useNavigate} from "react-router-dom";
-import {Hanko, register} from "@teamhanko/hanko-elements";
-
-
-const hankoUrl = "https://ebc6b9fe-ca5f-4256-8ef9-a3907104f110.hanko.io"
-
-export default function HankoAuth() {
-    const navigate = useNavigate();
-    const hanko = useMemo(() => new Hanko(hankoUrl), []);
-
-    const redirectAfterLogin = useCallback(() => {
-        navigate("/home");
-    }, [navigate]);
-
-    useEffect(
-        () =>
-            hanko.onAuthFlowCompleted(() => {
-                redirectAfterLogin();
-            }),
-        [hanko, redirectAfterLogin]
-    );
-
-    useEffect(() => {
-        register(hankoUrl).catch((error) => {
-            // handle error
-        });
-    }, []);
-
-    return <hanko-auth/>;
-}
+// import {useEffect, useCallback, useMemo} from "react";
+// import {useNavigate} from "react-router-dom";
+// import {Hanko, register} from "@teamhanko/hanko-elements";
+//
+// const hankoUrl = "https://ebc6b9fe-ca5f-4256-8ef9-a3907104f110.hanko.io"
+//
+// export default function HankoAuth() {
+//     const navigate = useNavigate();
+//     const hanko = useMemo(() => new Hanko(hankoUrl), []);
+//
+//     const redirectAfterLogin = useCallback(() => {
+//         navigate("/home");
+//     }, [navigate]);
+//
+//     useEffect(
+//         () =>
+//             hanko.onAuthFlowCompleted(() => {
+//                 redirectAfterLogin();
+//             }),
+//         [hanko, redirectAfterLogin]
+//     );
+//
+//     useEffect(() => {
+//         register(hankoUrl).catch((error) => {
+//             // handle error
+//         });
+//     }, []);
+//
+//     return <hanko-auth />;
+// }
